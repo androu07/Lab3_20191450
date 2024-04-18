2d05890afb1128f24f837cfce112aba803ceb5eb
andres <a20191450@pucp.edu.pe>
2024-04-17 19:39:39 -0500

    navbar

---
+++ src/main/java/com/example/lab03/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.lab03.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+    @GetMapping("/index" )
+    public String unaPersona(){
+        return "index";
+    }
+}
